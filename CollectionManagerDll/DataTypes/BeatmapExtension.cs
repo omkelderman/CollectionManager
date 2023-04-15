aa03576694cc1bcf9d636ba2f5b0df806826c561
Olle Kelderman <omkelderman@gmail.com>
2023-04-15 15:25:44 +0200

    dont alloc dict if not needed

--- CollectionManagerDll/DataTypes/BeatmapExtension.cs
+++ CollectionManagerDll/DataTypes/BeatmapExtension.cs
@@ -17,29 +17,34 @@ namespace CollectionManager.DataTypes
 
         #region Custom Field Stuff
 
-        private Dictionary<string, object> _customFields = new();
+        private Dictionary<string, object> _customFields;
 
         public void SetCustomFieldValues(BeatmapExtension other)
         {
-            _customFields = new Dictionary<string, object>();
-            foreach(var kvp in other._customFields)
+            if(other._customFields == null)
             {
-                _customFields.Add(kvp.Key, kvp.Value);
+                _customFields = null;
+            } else
+            {
+                _customFields = new Dictionary<string, object>(other._customFields);
             }
         }
 
         public void SetCustomFieldValue(string key, object value)
         {
+            _customFields ??= new Dictionary<string, object>();
             _customFields[key] = value;
         }
 
         public object GetCustomFieldValue(string key)
         {
+            if(_customFields == null ) return null;
             return _customFields.TryGetValue(key, out var value) ? value : null;
         }
 
         public IEnumerable<string> GetAllStringCustomFieldValues()
         {
+            if (_customFields == null) yield break;
             foreach(var customField in _customFields)
             {
                 if(customField.Value is string stringValue) yield return stringValue;
